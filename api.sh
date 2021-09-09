8754c976a6367c8159d2ae469941af15f7a682ca
Adarsh Anand <adarshanshu7@gmail.com>
2021-09-09 16:27:56 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -2,31 +2,32 @@
 
 adddate() {
     while IFS= read -r line; do
-        printf '%s %s\n' "$(date "+%Y-%m-%d %H:%M:%S")" "$line";
+        echo '%s %s\n' "$(date "+%Y-%m-%d %H:%M:%S")" "$line";
     done
 }
 
-ip=${1}
+ip=20.69.169.201
 
-echo "Cluster IP: ${ip}" >> adddate() >> samsung.log
-echo "curl -X GET --header 'Accept: application/json' 'https://${ip}/api/serverInfo'" >> adddate() >> samsung.log
-curl -X GET --header 'Accept: application/json' 'https://${ip}/api/serverInfo' >> samsung.json
+echo "Cluster IP: 20.69.169.201" >> adddate >> samsung.log
+echo "curl -X GET --header 'Accept: application/json' -k 'https://20.69.169.201/api/serverInfo'" >> adddate >> samsung.log
+curl -X GET --header 'Accept: application/json' -k 'https://20.69.169.201/api/serverInfo' >> samsung.json
 
 N=0
-echo "while [ $N -ne 1 ]" >> adddate() >> samsung.log
+echo "while [ $N -ne 1 ]" >> adddate >> samsung.log
 while [ $N -lt 1 ]
 do
-   echo "curl -X GET --header 'Accept: application/json' 'https://${ip}/api/serverInfo'" >> adddate() >> samsung.log
-   curl -X GET --header 'Accept: application/json' 'https://${ip}/api/serverInfo' >> samsung.json
+   echo "curl -X GET --header 'Accept: application/json' -k 'https://20.69.169.201/api/serverInfo'" >> adddate >> samsung.log
+   curl -X GET --header 'Accept: application/json' -k 'https://20.69.169.201/api/serverInfo' >> samsung.json
    if grep -o ACTIVE samsung.json
    then
-   echo "Cluster state is ACTIVE !!" >> adddate() >> samsung.log
+   echo "Cluster state is ACTIVE !!" >> adddate >> samsung.log
    N=2
    else
-   echo "sleeping for 10 sec" >> adddate() >> samsung.log
+   echo "sleeping for 10 sec" >> adddate >> samsung.log
    sleep 10
    fi
 done
 
-echo "Ending While Statement" >> adddate() >> samsung.log
-echo "Done" >> adddate() >> samsung.log
+rm samsung.json
+echo "Ending While Statement" >> adddate >> samsung.log
+echo "Done" >> adddate >> samsung.log
